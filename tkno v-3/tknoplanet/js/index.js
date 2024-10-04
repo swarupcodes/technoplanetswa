f91b4774276412b12f16ae094781c0752d1be0c8
Swarup <152853094+swarupcodes@users.noreply.github.com>
2024-10-04 11:22:09 +0545

    Delete tkno v-3/tknoplanet directory

--- tkno v-3/tknoplanet/js/index.js
+++
@@ -1,9 +0,0 @@
-
-function fadeContent() {
-    $(".slider_contents div:hidden:first-child").fadeIn(2500, function () {
-        $(this).fadeOut(2000, function () {
-            $(this).appendTo($(this).parent());
-            fadeContent();
-        });
-    });
-}
